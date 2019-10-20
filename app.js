f666a33ebebb7119b016e9214609e8779045ac6e
Nikhil Alex <nikhil.wtp@gmail.com>
2019-10-21 01:47:39 +0530

    working adding list

--- app.js
+++ app.js
@@ -19,57 +19,18 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 });
 
-const list = [];
+let list = [];
 
 class Txn {
   constructor(type, desc, amt, date) {
     this.type = type;
     this.desc = desc;
     this.amt = amt;
-    this.date = date !== '' ? new Date(date) : new Date();
+    this.date = date;
   }
 }
 
 class UI {
-  // static addTxnToList(doc) {
-  //   const txnList = document.getElementById('txn-list');
-  //   txnList.innerHTML += `
-  //   <div class="col s12 m6">
-  //   <div class="card-panel teal center-align">
-  //     <div class="row">
-  //       <div class="col s6">
-  //           <span class="white-text">${doc
-  //             .data()
-  //             .date.toDate()
-  //             .toDateString()}</span>
-  //       </div>
-  //       <div class="col s4">
-  //           <span class="white-text">${doc.data().type}</span>
-  //       </div>
-  //       <div class="col s2 right">
-  //           <span><a href="#" class="edit-txn" data-id="${
-  //             doc.id
-  //           }"><i class="material-icons white-text">edit</i><a></span>
-  //       </div>
-  //     </div>
-  //     <div class="row">
-  //       <div class="col s6">
-  //           <span class="white-text">${doc.data().desc}</span>
-  //       </div>
-  //       <div class="col s4">
-  //           <span class="white-text">${doc.data().amt}</span>
-  //       </div>
-  //       <div class="col s2 right">
-  //           <span><a href="#" class="delete-txn" data-id="${
-  //             doc.id
-  //           }"><i class="material-icons white-text">delete</i><a></span>
-  //       </div>
-  //     </div>
-  //   </div>
-  //   </div>
-  //   `;
-  // }
-
   static displayList() {
     let income = 0;
     let expense = 0;
@@ -182,8 +143,9 @@ document.getElementById('txn-form').addEventListener('submit', function(e) {
   } else if (date === '' && time !== '') {
     M.toast({ html: 'Please choose a date!', classes: 'rounded red' });
   } else {
+    const newDate = date === '' ? new Date() : new Date(`${date} ${time}`);
     // Instantiate txn
-    const txn = new Txn(type, desc, amt, `${date} ${time}`);
+    const txn = new Txn(type, desc, amt, newDate);
     Store.addTxn(txn);
     // Show success
     M.toast({ html: 'Transaction Added!', classes: 'rounded green' });
@@ -228,9 +190,7 @@ db.collection('txns').onSnapshot(snapshot => {
         id: change.doc.id,
       });
     } else if (change.type === 'removed') {
-      document
-        .querySelector(`[data-id="${change.doc.id}"]`)
-        .parentElement.parentElement.parentElement.parentElement.parentElement.remove();
+      list = list.filter(txn => txn.id !== change.doc.id);
     }
     UI.displayList();
   });
